0998613e12977694d354042e50d4471c8f6281b1
Henry Miskin <henry.miskin@talktalk.net>
2016-09-05 04:40:18 +0200

    Added further cities and updated scraper

--- main.py
+++ main.py
@@ -10,38 +10,42 @@ def parse_page(name):
 
     soup = BeautifulSoup(webpage, 'html.parser')
 
-    locations = []
+    waypoints = []
 
     for column in soup.find_all('div',class_='col-sm-4'):
 
-        location = {}
+        waypoint = {}
 
         feature = column.find('div',class_='tux-feature')
 
         main_content = feature.find('div',class_='feature-title')
-        location['name'] = main_content.find('h3').get_text()
-        location['category'] = main_content.find('h4').get_text()
+        waypoint['name'] = main_content.find('h3').get_text()
+        waypoint['category'] = main_content.find('h4').get_text()
 
         photo_content = feature.find('a',class_='feature-photo')
         photo_match = re.search(r"url\('//(.[^']*)'\);",str(photo_content))
 
         if (photo_match):
-            location['photo'] = 'http://' + photo_match.group(1)
+            waypoint['photo'] = 'http://' + photo_match.group(1)
 
         map_content = column.find('div',class_='map-marker')
         position_match = re.search(r'data-lat="([^"]*)" data-lng="([^"]*)',str(map_content))
 
         if (position_match):
-            location['point'] = {
+            waypoint['point'] = {
                 'lat': float(position_match.group(1)),
                 'lng': float(position_match.group(2))
             }
 
-        locations.append(location)
+        waypoints.append(waypoint)
 
-    json.dump(locations,open('data/{}.json'.format(name),'w'),indent=4)
+    json.dump(waypoints,open('data/{}.json'.format(name),'w'),indent=4)
+
+    return waypoints
 
 def find_pages():
+
+    waypoints = []
 
     filepaths = glob.glob('data/*.html')
 
@@ -50,7 +54,9 @@ def find_pages():
         match = re.match(r'data/([^.]*)\.html',filepath)
         name = match.group(1)
 
-        parse_page(name)
+        waypoints.extend(parse_page(name))
+
+    json.dump(waypoints,open('data/waypoints.json','w'),indent=4)
 
 if (__name__ == "__main__"):
     find_pages()
