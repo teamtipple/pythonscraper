96bace8d0cbe3eb5d9f12bc0cef440e9a019a818
Henry Miskin <henry.miskin@talktalk.net>
2016-09-08 09:58:22 +0200

    Added cologne pubs to repo

--- main.py
+++ main.py
@@ -22,6 +22,13 @@ def parse_page(name):
         waypoint['name'] = main_content.find('h3').get_text()
         waypoint['category'] = main_content.find('h4').get_text()
 
+        path = re.search(r'href="(/[^"]*)',str(main_content)).group(1)
+        waypoint['website'] = 'http://www.triposo.com/{}'.format(path)
+
+        waypoint['icon'] = 'Multiple'
+        waypoint['filter'] = ['Cocktails', 'Beer', 'Spirits', 'Wine']
+        waypoint['description'] = ''
+
         photo_content = feature.find('a',class_='feature-photo')
         photo_match = re.search(r"url\('//(.[^']*)'\);",str(photo_content))
 
@@ -32,14 +39,17 @@ def parse_page(name):
         position_match = re.search(r'data-lat="([^"]*)" data-lng="([^"]*)',str(map_content))
 
         if (position_match):
-            waypoint['point'] = {
+            waypoint['geo'] = {
                 'lat': float(position_match.group(1)),
-                'lng': float(position_match.group(2))
+                'long': float(position_match.group(2))
             }
 
         waypoints.append(waypoint)
 
     json.dump(waypoints,open('output/{}.json'.format(name),'w'),indent=4)
+
+    categories = list(set([waypoint['category'] for waypoint in waypoints]))
+    json.dump(categories,open('output/categories.json','w'))
 
     return waypoints
 
@@ -56,15 +66,19 @@ def find_pages():
 
         waypoints.extend(parse_page(name))
 
-    categories = json.load(open('input/categories.json'))
 
-    for waypoint in waypoints:
-        waypoint['icon'] = 'client/assets/images/{}'.format(
-            categories[waypoint['category']]
-        )
+    categories = list(set([waypoint['category'] for waypoint in waypoints]))
+    json.dump(categories,open('output/categories.json','w'))
+    # categories = json.load(open('input/categories.json'))
+
+    # for waypoint in waypoints:
+    #     waypoint['icon'] = 'client/assets/images/{}'.format(
+    #         categories[waypoint['category']]
+    #     )
 
     json.dump(waypoints,open('output/waypoints.json','w'),indent=4)
 
 
 if (__name__ == "__main__"):
-    find_pages()
+    # find_pages()
+    parse_page('cologne_pubs')
